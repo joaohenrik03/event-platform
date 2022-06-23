ab321d692fd488016c77bb3b6016e14b581a3594
João Henrik <jh.signori@gmail.com>
2022-06-23 17:26:13 -0300

    added Player.tsx and SideBar.tsx component to Event.tsx

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,9 +1,15 @@
 import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+import { SideBar } from "../components/SideBar";
 
 export function Event() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header />
-        </>
+            <main className="flex flex-1">
+                <Player />
+                <SideBar />
+            </main>
+        </div>
     )
 }
