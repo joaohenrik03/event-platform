ce1f7052e8de9fde43b0688a110c0a12b3af9f53
João Henrik <jh.signori@gmail.com>
2022-06-23 17:01:53 -0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './lib/apollo'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './lib/apollo';
 
-import './styles/global.css'
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
     </ApolloProvider>
   </React.StrictMode>
-)
+);
