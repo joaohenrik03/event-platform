6876167b50641c0b4194ec225eee8c66bda9cea7
João Henrik <jh.signori@gmail.com>
2022-06-23 17:28:21 -0300

    Added info and styles to SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,9 @@
 export function SideBar() {
     return (
-        <div>
-            sidebar
-        </div>
+        <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+                Cronograma de Aulas
+            </span>
+        </aside>
     )
 }
