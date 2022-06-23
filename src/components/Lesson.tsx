d14f491cd3bbcf48bc540a5886f61d85e20d82cc
João Henrik <jh.signori@gmail.com>
2022-06-23 17:03:18 -0300

    created components

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,7 @@
+export function Lesson() {
+    return (
+        <div>
+            lesson
+        </div>
+    );
+};
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,7 @@
+export function SideBar() {
+    return (
+        <div>
+            sidebar
+        </div>
+    );
+};
