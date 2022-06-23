3fa50179ac06675d4d796861f4ba4e37246867e2
João Henrik <jh.signori@gmail.com>
2022-06-23 18:19:40 -0300

    Typing Lesson.tsx

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,42 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
 
-export function Lesson() {
+type LessonProps = {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = true;
+
     return (
         <a href="#">
             <span className="text-gray-300 ">
-                Domingo • 20 de junho • 19h00
+                {props.availableAt.toString()}
             </span>
 
             <div className="rounded border border-gray-500 p-4 mt-2">
                 <header className="flex items-center justify-between">
-                    <span className="tex-sm text-blue-500 font-medium flex items-center gap-2">
-                        <CheckCircle size={20}/>
-                        Conteúdo liberado
-                    </span>
+                    {isLessonAvailable ? (
+                        <span className="tex-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20}/>
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="tex-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20}/>
+                            Em breve
+                        </span>
+                    )}
+
                     <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold uppercase">
-                        Ao vivo
+                        {props.type === 'live' ? 'Ao vivo' : 'Aula Prática'}
                     </span>
                 </header>
 
                 <strong className="text-gray-200 mt-5 block">
-                    Abertura do evento Ignite labs
+                    {props.title}
                 </strong>
             </div>
         </a>
