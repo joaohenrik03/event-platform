395361fbbbe7e2f167a9e91c0e8b3fffa3742d5f
João Henrik <jh.signori@gmail.com>
2022-06-24 00:42:31 -0300

    Footer added

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -2,6 +2,7 @@ import { Header } from "../components/Header";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { SideBar } from "../components/SideBar";
 import { useParams } from "react-router-dom";
+import { Footer } from "../components/Footer";
 
 type Params = {
     slug: string;
@@ -15,8 +16,9 @@ export function Event() {
             <Header />
             <main className="flex flex-1">
                 {slug ? <VideoPlayer lessonSlug={slug}/> : <div className="flex-1"></div>}
-                <SideBar />
+                <SideBar />          
             </main>
+            {slug ? <div></div> : <Footer />}
         </div>
     )
 }
