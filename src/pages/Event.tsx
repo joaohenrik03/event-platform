5b72f8642a773b61f37868648899e0643cc88ef0
João Henrik <jh.signori@gmail.com>
2022-06-23 23:04:10 -0300

    Added room route via slug

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,20 @@
 import { Header } from "../components/Header";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { SideBar } from "../components/SideBar";
+import { useParams } from "react-router-dom";
+
+type Params = {
+    slug: string;
+}
 
 export function Event() {
+    const { slug } = useParams<Params>() 
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex flex-1">
-                <VideoPlayer />
+                {slug ? <VideoPlayer lessonSlug={slug}/> : <div className="flex-1"></div>}
                 <SideBar />
             </main>
         </div>
