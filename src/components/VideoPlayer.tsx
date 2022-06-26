28e9326e2075aff75928d458ee5653b96615cb73
João Henrik <jh.signori@gmail.com>
2022-06-26 02:03:01 -0300

    Updated VideoPlayers component buttons

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -58,7 +58,7 @@ export function VideoPlayer(props: VideoProps) {
                     </div>
 
                     <div className="flex flex-col gap-4">
-                        <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+                        <a href="https://discord.com/oauth2/authorize?client_id=949331074079662180&redirect_uri=https%3A%2F%2Fdiscord-service.rocketseat.dev%2Fsignin%2Fdynamic-callback&response_type=code&scope=identify+email+guilds.join&state=ignite-lab" target="_blank" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                             <DiscordLogo size={24}/>
                             Comunidade do Discord
                         </a>
@@ -71,7 +71,7 @@ export function VideoPlayer(props: VideoProps) {
                 </div>
 
                 <div className="gap-8 mt-20 grid grid-cols-2">
-                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <a href="https://efficient-sloth-d85.notion.site/Material-complementar-86d4ef35af16471ebc3ae3eba1a378e5" target="_blank" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40}/>
                         </div>
@@ -86,7 +86,7 @@ export function VideoPlayer(props: VideoProps) {
                         </div>
                     </a>
 
-                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <a href="https://drive.google.com/drive/folders/1mxWnvlqmH7MbVRv2Na9xFNgCQCygM1iR" target="_blank" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <Image size={40}/>
                         </div>
