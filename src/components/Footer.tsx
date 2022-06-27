138228c9c18cead3a0fbf5bee03b3f364bee6efc
João Henrik <jh.signori@gmail.com>
2022-06-26 22:49:42 -0300

    Improved footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,14 @@ import { LogoRocket } from "./LogoRocket";
 
 export function Footer() {
     return (
-        <footer className="mx-6 pb-5 pt-6 flex items-center justify-between border-t border-gray-600 w-[calc(100%-3rem)] bg-black">
-            <div className="flex items-center gap-6">
+        <footer className="mx-2 md:mx-6 pb-5 pt-6 flex items-center justify-between border-t border-gray-600 w-[calc(100%-1rem)] md:w-[calc(100%-3rem)] bg-black">
+            <div className="flex flex-col md:flex-row items-left gap-2 md:gap-6">
                 <LogoRocket />
-                <span className="text-base text-gray-300">Rocketseat - Todos os direitos reservados</span>
+                <span className="text-xs md:text-base text-gray-300">Rocketseat - Todos os direitos reservados</span>
             </div>
 
             <div>
-                <a href="#" className="text-base text-gray-300">
+                <a href="#" className="text-xs md:text-base text-gray-300">
                     Políticas de privacidade
                 </a>
             </div>
