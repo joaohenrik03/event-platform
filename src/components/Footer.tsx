21658cf90ce09d88d49125de1040f9a82007fae3
João Henrik <jh.signori@gmail.com>
2022-07-06 23:31:57 -0300

    Updated styling for better responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { LogoRocket } from "./LogoRocket";
 
 export function Footer() {
     return (
-        <footer className="mx-2 md:mx-6 pb-5 pt-6 flex items-center justify-between border-t border-gray-600 w-[calc(100%-1rem)] md:w-[calc(100%-3rem)] bg-black">
-            <div className="flex flex-col md:flex-row items-left gap-2 md:gap-6">
+        <footer className="bg-black mx-2 md:mx-6 pb-5 pt-6 flex flex-col md:flex-row items-center gap-4 border-t border-gray-600 w-[calc(100%-1rem)] md:w-[calc(100%-3rem)] md:justify-between">
+            <div className="flex flex-col items-center gap-4 md:items-start">
                 <LogoRocket />
                 <span className="text-xs md:text-base text-gray-300">Rocketseat - Todos os direitos reservados</span>
             </div>
